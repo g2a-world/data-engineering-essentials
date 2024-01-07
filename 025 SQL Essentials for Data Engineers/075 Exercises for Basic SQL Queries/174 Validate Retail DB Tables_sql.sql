6ab0613510066bd2999c1ffede1b8ea5127d73ff
g2a-world <g2a-world>
2024-01-07 23:01:20 +0000

    checking in code

---
+++ 025 SQL Essentials for Data Engineers/075 Exercises for Basic SQL Queries/174 Validate Retail DB Tables_sql.sql
@@ -0,0 +1,7 @@
+-- Databricks notebook source
+SELECT count(*) FROM departments;
+SELECT count(*) FROM categories;
+SELECT count(*) FROM products;
+SELECT count(*) FROM customers;
+SELECT count(*) FROM orders;
+SELECT count(*) FROM order_items;
